ae29602279f409eb2b746d4c589604fd09afb19f
Thiago Kpelo <thiago.kpelo@loft.com.br>
2020-04-19 18:17:41 -0300

    refactor: title align of charts

--- src/components/charts/RiskScatterPlot/index.js
+++ src/components/charts/RiskScatterPlot/index.js
@@ -8,7 +8,7 @@ import AreaLayer from '../AreaLayer';
 const commonProps = {
   width: 300,
   height: 200,
-  margin: { top: 10, right: 10, bottom: 25, left: 20 },
+  margin: { top: 10, right: 30, bottom: 25, left: 10 },
   nodeSize: 5,
   blendMode: 'multiply',
   yScale: {
@@ -16,27 +16,30 @@ const commonProps = {
     max: 5,
   },
   xFormat: (d) => d,
-  yFormat: (d) => d,
+  yFormat: (d) => d.toFixed(1),
   enableGridX: false,
   axisBottom: {
+    type: 'time',
     tickValues: 5,
     orient: 'bottom',
     tickSize: 5,
     tickPadding: 5,
-    tickRotation: 90,
+    tickRotation: -90,
     format: (d) => d,
   },
-  axisLeft: {
+  axisTop: null,
+  axisLeft: null,
+  axisRight: {
     orient: 'left',
     tickSize: 5,
     tickValues: 5,
     tickPadding: 5,
     tickRotation: 0,
-    format: (d) => d,
+    format: (d) => d.toFixed(1),
   },
   legends: [
     {
-      anchor: 'bottom-left',
+      anchor: 'bottom-right',
       direction: 'row',
       translateY: 60,
       itemWidth: 130,
@@ -59,17 +62,8 @@ const RiskScatterPlot = ({ data }) => (
 
 const createRiskList = (data) =>
   Object.keys(data).map((id) => (
-    <Grid
-      alignItems="center"
-      justify="center"
-      container
-      direction="column"
-      item
-      key={id}
-      lg={3}
-      xs
-    >
-      <Typography variant="h5">{id}</Typography>
+    <Grid container direction="column" item key={id} lg={3} xs>
+      <Typography variant="h6">{id}</Typography>
       <RiskScatterPlot data={[{ id, data: data[id] }]} />
     </Grid>
   ));
