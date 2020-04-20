6aeafdb314506b2e38cc9a6e24fa867cd194ff93
Thiago Kpelo <thiago.kpelo@loft.com.br>
2020-04-20 12:22:56 -0300

    refactor: new color of rt line

--- src/components/charts/LineLayer/index.js
+++ src/components/charts/LineLayer/index.js
@@ -6,7 +6,7 @@ const LineLayer = ({ nodes, xScale, yScale }) => {
     .x((d) => xScale(d.data.x))
     .y((d) => yScale(1));
 
-  return <path stroke="#666" strokeWidth="2" d={avgline(nodes)} />;
+  return <path stroke="red" strokeWidth="1" d={avgline(nodes)} />;
 };
 
 export default LineLayer;
