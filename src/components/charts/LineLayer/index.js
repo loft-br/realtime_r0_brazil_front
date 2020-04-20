457395df7ffce297cd1614b994ffc00239ed8b0c
Thiago Kpelo <thiago.kpelo@loft.com.br>
2020-04-19 21:48:00 -0300

    refactor: charts and title page

---
+++ src/components/charts/LineLayer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { line } from 'd3-shape';
+
+const LineLayer = ({ nodes, xScale, yScale }) => {
+  const avgline = line()
+    .x((d) => xScale(d.data.x))
+    .y((d) => yScale(1));
+
+  return <path stroke="#666" stroke-width="2" d={avgline(nodes)} />;
+};
+
+export default LineLayer;
