31420da8001bc7d36a5bac95945a56d4539d9903
Filipe Cunha <filipe.cunha@loft.com.br>
2020-04-20 14:19:50 -0300

    Redux integrated

---
+++ src/store/reducers.js
@@ -0,0 +1,41 @@
+import actionTypes from './action_types';
+
+const inicialState = {
+  // state_list: null,
+  // model_result_state: null,
+  // last_date: null,
+  data: null,
+  lastUpdateTime: null,
+}
+
+export default (state = inicialState, action) => {
+  switch (action.type) {
+    // case actionTypes.GET_STATE_LIST :
+    //   return {
+    //     ...state,
+    //     state_list: action.payload
+    //   }
+    //
+    // case actionTypes.GET_MODEL_RESULT_STATE :
+    //   return {
+    //     ...state,
+    //     model_result_state: action.payload
+    //   }
+    //
+    // case actionTypes.GET_LAST_DATE :
+    //   return {
+    //     ...state,
+    //     last_date: action.payload
+    //   }
+
+    case actionTypes.GET_MODEL_RESULTS :
+      return {
+        ...state,
+        data: action.payload.data,
+        lastUpdateTime: action.payload.lastUpdateTime,
+      }
+
+    default:
+      return state;
+  }
+}
