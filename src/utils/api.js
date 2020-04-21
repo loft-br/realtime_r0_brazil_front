1b8893230603b508b42da95fee6f1a4b8aa181c9
Thiago Capelo <thiagokpelo@gmail.com>
2020-04-21 11:20:28 -0300

    refactor: eslint syntax

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://realtime-r0-covid-brazil.herokuapp.com',
   headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
-    "Content-Type": "application/json;charset=UTF-8"
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    'Content-Type': 'application/json;charset=UTF-8',
   },
 });
 
@@ -17,7 +17,7 @@ export const post = (path, data) =>
 
 export const put = (path, data) => axiosInstance(path, { method: 'PUT', data });
 
-export const remove = path => axiosInstance(path, { method: 'DELETE' });
+export const remove = (path) => axiosInstance(path, { method: 'DELETE' });
 
 export const upload = (path, formattedFormData) =>
   axiosInstance(path, {
