8ac74d4e39ef0432c05209dc7b7d74471d05bf4d
Thiago Kpelo <thiago.kpelo@loft.com.br>
2020-04-24 18:50:25 -0300

    chore(code_style): linter

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import reducers from './reducers';
 
 const persistConfig = {
   key: 'loft-science',
   storage,
-}
+};
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
-  let store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)))
-  let persistor = persistStore(store)
-  return { store, persistor }
-}
+  let store = createStore(
+    persistedReducer,
+    composeEnhancers(applyMiddleware(thunk))
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
