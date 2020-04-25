e7688912a9f2c019cdc35c606a9be4a5d51f559c
Thiago Kpelo <thiago.kpelo@loft.com.br>
2020-04-25 12:29:12 -0300

    style: remove loading wrapper of Home styles

--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -1,20 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  loadingWrapper: {
-    width: '100vw',
-    height: '110vh',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    backgroundColor: 'white',
-    zIndex: 999,
-    margin: '0 auto',
-    textAlign: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   errorWrapper: {
     width: '100%',
     margin: '0 auto',
