098a831c2e0b165a7a1d7bbe35f7d5a182467f5d
Thiago Capelo <thiagokpelo@gmail.com>
2020-04-26 11:39:31 -0300

    refactor(cases_line):  add cases line graph

--- src/components/charts/CasesLine/TooltipCaseLine/index.js
+++ src/components/charts/CasesLine/TooltipCaseLine/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
-import { Typography } from '@material-ui/core';
+import { Typography, Paper } from '@material-ui/core';
 import { formatDate } from 'utils';
-import Tooltip from 'components/charts/Tooltip';
 
-const TooltipCaseLine = ({ data }) => (
-  <Tooltip data={data}>
-    <Typography variant="caption">
-      <strong>Dia {formatDate(data?.date)}</strong>
-    </Typography>
-  </Tooltip>
-);
+import useStyles from './TooltipCaseLine.styles';
+
+const TooltipCaseLine = ({ data = {} }) => {
+  const classes = useStyles();
+
+  return (
+    <Paper variant="outlined" elevation={3} className={classes.root}>
+      <Typography variant="caption">
+        <strong>Dia {formatDate(data?.date)}</strong>
+      </Typography>
+      <br />
+      <Typography variant="caption">
+        Novos casos: <strong>{data?.y}</strong> (7d)
+      </Typography>
+    </Paper>
+  );
+};
 
 export default TooltipCaseLine;
